e00374afa80d2d286f95fc50b488e12b1601af00
Sandeep Risal <sandeep.risal@ekbana.info>
2024-02-02 14:52:32 +0545

    fix: commented language select code

--- src/shared/main-layout/header/index.tsx
+++ src/shared/main-layout/header/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Logo } from "@/shared/lib/image-config";
 import LanguageToggler from "./language-toggler";
-// import { version } from "../../../../version";
+import { version } from "../../../../version";
 // import { useProfileStore } from "@/store/profile-store";
 interface IHeaderProps {
   isExpanded: boolean;
@@ -71,10 +71,10 @@ const Header = ({
         <div className="flex items-center gap-6">
           {/* <ThemeToggler /> */}
           {/* <InputSearch /> */}
-          {/* <p className="text-xs text-zinc-700">v {version}</p> */}
+          <p className="text-xs text-zinc-700">v {version}</p>
           <ProfileDropdown />
+          {/* <LanguageToggler /> */}
         </div>
-        <LanguageToggler />
       </div>
     </header>
   );

37940bf067e75c9140ec7d9552b6acb2016beb82
Sandeep Dangol <sandeep.dangol@ekbana.net>
2024-02-08 13:35:40 +0545

    fix(project-detail): skeleton Loading added

--- src/pages/projects/[id]/index.tsx
+++ src/pages/projects/[id]/index.tsx
@@ -1,13 +1,19 @@
+import DetailContent from "@/features/Projects/detail";
 import MainLayout from "@/shared/main-layout";
 import { getI18nProps } from "@/shared/utils/i18n-utils/i18n.util";
 import React from "react";
 
 const ProjectDetail = () => {
-  return <div>ProjectDetail</div>;
+  return (
+    <div>
+      <DetailContent />
+    </div>
+  );
 };
-// export const getStaticProps = getI18nProps;
 
 export default ProjectDetail;
+// export const getStaticProps = getI18nProps;
+
 ProjectDetail.getLayout = (page: any) => {
   return <MainLayout title="Projects">{page}</MainLayout>;
 };

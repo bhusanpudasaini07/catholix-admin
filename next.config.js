6203e328b276c2a37ccb9b97c802e3474a904645
Sandeep Risal <sandeep.risal@ekbana.info>
2024-02-12 10:15:40 +0545

    fix: log error fixes

--- next.config.js
+++ next.config.js
@@ -3,10 +3,23 @@ const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   images: {
-    domains: ["system.dev-orion.ekbana.info", "system.ek-orion.ekbana.info"],
+    domains: [""],
   },
   reactStrictMode: true,
   i18n,
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.plugins = [
+      ...config.plugins,
+      new webpack.DefinePlugin({
+        __REACT_DEVTOOLS_GLOBAL_HOOK__: "({ isDisabled: true })",
+      }),
+    ];
+    // Important: return the modified config
+    return config;
+  },
 };
 
 module.exports = nextConfig;

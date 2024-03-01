39d7d218c3cbd441f626ac228168ea0dde1ac56b
Sandeep Dangol <sandeep.dangol@ekbana.net>
2024-03-01 09:09:00 +0545

    feat(trendline): table data

--- src/features/Team/team-leads/lead-report/trending-graph/index.tsx
+++ src/features/Team/team-leads/lead-report/trending-graph/index.tsx
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import TrendingGraphBody from "./trending-graph-body";
 import TrendingGraphHeader from "./trending-graph-header";
 import { DateRange } from "react-day-picker";
+import { useRouter } from "next/router";
 
 const TrendingGraphContent = () => {
+  const router = useRouter();
+  const current_id = router.query?.lead_id || undefined;
+
   const oneWeekAgo = new Date();
   oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
   const [dateRange, setDateRange] = useState<DateRange | undefined>({
@@ -14,7 +18,11 @@ const TrendingGraphContent = () => {
   return (
     <div>
       <TrendingGraphHeader dateRange={dateRange} setDateRange={setDateRange} />
-      <TrendingGraphBody />
+      <TrendingGraphBody
+        id={current_id}
+        end_date={dateRange?.to}
+        start_date={dateRange?.from}
+      />
     </div>
   );
 };

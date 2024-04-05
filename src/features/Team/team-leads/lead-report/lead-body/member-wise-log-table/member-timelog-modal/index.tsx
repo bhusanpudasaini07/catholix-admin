e554bab4de6f411b805a26b106a4a1f50f6b2dfe
Sandeep Risal <sandeep.risal@ekbana.info>
2024-04-05 17:44:06 +0545

    fix: redmine improvement added (#13170).

--- src/features/Team/team-leads/lead-report/lead-body/member-wise-log-table/member-timelog-modal/index.tsx
+++ src/features/Team/team-leads/lead-report/lead-body/member-wise-log-table/member-timelog-modal/index.tsx
@@ -13,6 +13,8 @@ import {
   IMemberLogTaskData,
   IMembersLogData,
 } from "@/interface/team-lead-report-interface";
+import { Badge } from "@/shared/components/ui/badge";
+import { cn } from "@/shared/utils/utils";
 
 interface IProps {
   staffDailyLog: any;
@@ -88,59 +90,93 @@ const MemberTimeLogModal: FC<IProps> = ({
   return (
     <div className="flex flex-col max-h-[700px] overflow-auto">
       {staffDailyLogLoading && <TrendModalSkeleton />}
+      {/* <p className="mb-4 text-base font-bold text-zinc-700">
+        Daily Budget Detail -{" "}
+      </p> */}
       {staffDailyLog?.data?.map((daily: any, index: number) => (
         <div className="mb-4" key={index}>
-          <Card className="h-[200px] mb-2">
-            <CardContent>
-              <div className="flex items-center justify-start gap-3 mb-4">
-                <h5 className="font-medium text-zinc-700">{daily?.date}</h5>
+          <Card className="h-auto">
+            <CardContent className="!p-0">
+              <div className="p-4 2xl:p-6">
+                <div className="flex gap-3 justify-start items-center mb-4">
+                  <h5 className="font-medium text-zinc-700">{daily?.date}</h5>
+                  {(daily?.holiday === "Yes" || daily?.on_leave === "Yes") && (
+                    <Badge
+                      className={
+                        "bg-red-100 rounded-md border-red-500 text-destructive"
+                      }
+                    >
+                      {daily?.holiday === "Yes" ? "Holiday" : "On Leave"}
+                    </Badge>
+                  )}
+                </div>
+                {daily?.holiday !== "Yes" ? (
+                  daily?.task?.length > 0 ? (
+                    <div className="flex gap-5 justify-between pr-20 mt-9">
+                      <div className="flex gap-2 justify-center items-start">
+                        <div className="mt-0 text-blue-500">
+                          <Hourglass size={36} />
+                        </div>
+                        <div className="ml-1">
+                          <p className="text-3xl font-semibold text-blue-500">
+                            {/* {hours}H {minutes}M */}
+                            {calculateHoursAndMinutes(daily?.used_time)}
+                          </p>
+                          <p className="text-sm text-blue-600">
+                            Total Time Logged
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex gap-2 justify-center items-start">
+                        <div className="mt-0 text-green-500">
+                          <Activity size={36} />
+                        </div>
+                        <div className="ml-1">
+                          <p className="text-3xl font-semibold text-green-500">
+                            {daily?.used_rp}
+                          </p>
+                          <p className="text-sm font-normal text-green-600">
+                            Spent Budget
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex gap-2 justify-center items-start">
+                        <div className="mt-0 text-red-500">
+                          <TrendingDown size={36} />
+                        </div>
+                        <div className="ml-1">
+                          <p className="text-3xl font-semibold text-red-500">
+                            {daily?.loss_rp}
+                          </p>
+                          <p className="text-sm text-red-600">Loss Budget</p>
+                        </div>
+                      </div>
+                    </div>
+                  ) : daily?.on_leave === "Yes" ? (
+                    <p className="p-6 font-medium text-center text-zinc-500">
+                      No data found.
+                    </p>
+                  ) : (
+                    <p className="p-6 font-medium text-center text-zinc-500">
+                      No time-log Added.
+                    </p>
+                  )
+                ) : (
+                  <p className="p-6 font-medium text-center text-zinc-500">
+                    Holiday/Weekend
+                  </p>
+                )}
               </div>
-              <div className="flex justify-between gap-5 pr-20 mt-9">
-                <div className="flex items-start justify-center gap-2">
-                  <div className="mt-0 text-blue-500">
-                    <Hourglass size={36} />
-                  </div>
-                  <div className="ml-1">
-                    <p className="text-3xl font-semibold text-blue-500">
-                      {/* {hours}H {minutes}M */}
-                      {calculateHoursAndMinutes(daily?.used_time)}
-                    </p>
-                    <p className="text-sm text-blue-600">Total Time Logged</p>
-                  </div>
-                </div>
-                <div className="flex items-start justify-center gap-2">
-                  <div className="mt-0 text-green-500">
-                    <Activity size={36} />
-                  </div>
-                  <div className="ml-1">
-                    <p className="text-3xl font-semibold text-green-500">
-                      {daily?.used_rp}
-                    </p>
-                    <p className="text-sm font-normal text-green-600">
-                      Spent Budget
-                    </p>
-                  </div>
-                </div>
-                <div className="flex items-start justify-center gap-2">
-                  <div className="mt-0 text-red-500">
-                    <TrendingDown size={36} />
-                  </div>
-                  <div className="ml-1">
-                    <p className="text-3xl font-semibold text-red-500">
-                      {daily?.loss_rp}
-                    </p>
-                    <p className="text-sm text-red-600">Loss Budget</p>
-                  </div>
-                </div>
-              </div>
+              {daily?.holiday !== "Yes" && daily?.task?.length > 0 && (
+                <DataTable
+                  loading={staffDailyLogLoading}
+                  border
+                  columns={columns}
+                  data={daily?.task || []}
+                />
+              )}
             </CardContent>
           </Card>
-          <DataTable
-            loading={staffDailyLogLoading}
-            border
-            columns={columns}
-            data={daily?.task || []}
-          />
         </div>
       ))}
     </div>

7cc5879f2b796da0e7c03b0a96546431809f2d1e
Sandeep Risal <sandeep.risal@ekbana.info>
2024-04-08 17:46:25 +0545

    fix: conflict fixes

--- src/features/Team/team-leads/lead-report/lead-body/member-wise-log-table/member-timelog-modal/index.tsx
+++ src/features/Team/team-leads/lead-report/lead-body/member-wise-log-table/member-timelog-modal/index.tsx
@@ -14,10 +14,6 @@ import {
   IMembersLogData,
 } from "@/interface/team-lead-report-interface";
 import { Badge } from "@/shared/components/ui/badge";
-<<<<<<< HEAD
-=======
-import { cn } from "@/shared/utils/utils";
->>>>>>> dev
 
 interface IProps {
   staffDailyLog: any;

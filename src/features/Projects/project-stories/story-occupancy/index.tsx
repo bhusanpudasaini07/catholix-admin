9bd2f4da013a047613eb22a5f942deee09e992d2
Sandeep Risal <sandeep.risal@ekbana.info>
2024-04-23 14:27:51 +0545

    fix: skeleton added in graph

--- src/features/Projects/project-stories/story-occupancy/index.tsx
+++ src/features/Projects/project-stories/story-occupancy/index.tsx
@@ -3,9 +3,12 @@ import React from "react";
 
 import useProjectStories from "@/hooks/project/detail/useProjectStories.hook";
 import { Card, CardContent } from "@/shared/components/ui/card";
+import GraphSkeleton from "@/shared/components/skeleton-loading/graph-skeleton";
+import PieChartSkeleton from "@/shared/components/skeleton-loading/pie-chart-skeleton";
 
 const UserStoryOccupancy = () => {
-  const { storyOccupancyOption, occupancyChartRef } = useProjectStories();
+  const { storyOccupancyOption, occupancyChartRef, isLoading } =
+    useProjectStories();
   return (
     <Card>
       <CardContent>
@@ -16,11 +19,15 @@ const UserStoryOccupancy = () => {
         </div>
 
         <div>
-          <ReactEcharts
-            ref={occupancyChartRef}
-            option={storyOccupancyOption}
-            opts={{ renderer: "svg" }}
-          />
+          {isLoading ? (
+            <PieChartSkeleton height={250} width={250} />
+          ) : (
+            <ReactEcharts
+              ref={occupancyChartRef}
+              option={storyOccupancyOption}
+              opts={{ renderer: "svg" }}
+            />
+          )}
         </div>
       </CardContent>
     </Card>

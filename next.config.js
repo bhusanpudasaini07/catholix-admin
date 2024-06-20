420929cffcf9d28119b8d9e2dd72946dd6ae5b01
Sandeep Risal <sandeep.risal@ekbana.info>
2024-06-20 14:31:49 +0545

    fix: required validation schema fixed. email validation fixed. permission added in roles module

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   images: {
-    domains: ["localhost"],
+    domains: ["dev-api-mdmexpansion.ekbana.net"],
   },
   reactStrictMode: true,
   i18n: {
--- src/schema/auth-schema/login-schema.ts
+++ src/schema/auth-schema/login-schema.ts
@@ -3,12 +3,10 @@ import { z } from "zod";
 const LoginSchema = z.object({
   email: z
     .string({ required_error: "Email is required." })
+    .email({ message: "Please enter a valid email address." })
     .min(1, "Email is required.")
-    .max(50, "Email must not exceed 50 characters.")
-    .regex(
-      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-      "Please enter a valid email address."
-    ),
+    .max(50, "Email must not exceed 50 characters."),
+
   password: z.string({ required_error: "Password is required." }).min(1, {
     message: "Password is required.",
   }),
